91265dabfb2b8f4036757f6629ffdec91df3a342
Sefirr <sefir1223@gmail.com>
2016-01-17 14:19:27 +0100

    Añadida información de apps a la página

--- twitter.php
+++ twitter.php
@@ -1,1 +1,23 @@
-<?php echo 'Twitter'; ?>
+<?php 
+	
+	include_once 'config.php';
+	
+	global $BD;
+	$query = $BD->query("SELECT * FROM data_icon WHERE name LIKE 'twitter'");
+	$data_icon = $query->fetch_array();
+	
+	$data = explode(" ", $data_icon["text"]);
+	
+	$h = 0;
+	for($i=0;$i < count($data); $i++) {
+		if($h < 10) {
+				echo " ".$data[$i];
+		} else {
+			echo "<br>".$data[$i];
+			$h = 0;
+		}
+		$h++;
+	}
+	
+	echo "<div style='text-align:center'>Para saber más, pulse en el botón de arriba.</div>"; 
+?>
